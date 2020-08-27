022385e6ae4ee5754ec5c08792d2238c703f6f5a
JeongminYu <likemin0142@gmail.com>
2020-08-28 04:24:50 +0900

    change application.properties server.port

--- src/main/java/com/api/yirang/YirangApplication.java
+++ src/main/java/com/api/yirang/YirangApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class YirangApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(YirangApplication.class, args);
     }
