ae3cd3b135aae2bc92857f6c8dbd3e9e92990847
JeongminYu <likemin0142@gmail.com>
2020-08-27 22:59:39 +0900

    first commit

---
+++ src/main/java/com/api/yirang/YirangApplication.java
@@ -0,0 +1,13 @@
+package com.api.yirang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YirangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YirangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/yirang/YirangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.yirang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YirangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
