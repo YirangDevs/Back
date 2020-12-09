2415803be6d5ef57dcbc4597d1811e4c89ccd225
JeongminYu <likemin0142@gmail.com>
2020-12-09 22:47:36 +0900

    First

---
+++ src/main/java/com/api/yirang/apply/domain/model/Apply.java
@@ -0,0 +1,23 @@
+package com.api.yirang.apply.domain.model;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by JeongminYoo on 2020/12/9
+ * Work with Yirang
+ * Email: likemin0142@gmail.com
+ * Blog: http://Beewoom.github.io
+ * Github: http://github.com/Biewoom
+ */
+@Entity
+@Table(name = "apply")
+@Getter
+@ToString
+public class Apply {
+
+    
+}
